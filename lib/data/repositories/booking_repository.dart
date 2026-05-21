7ed3fadf856c6f6521b331dfaf3144388e04d358
Anmolpal099 <anmolpal156@gmail.com>
2026-05-21 12:07:09 +0545

    fix: add mock data fallback when backend unavailable - fixes DioException

--- lib/data/repositories/booking_repository.dart
+++ lib/data/repositories/booking_repository.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/booking_model.dart';
 import '../../services/api/api_client.dart';
 import '../../core/constants/api_constants.dart';
+import '../datasources/local/mock_data.dart';
 
 final bookingRepositoryProvider = Provider<BookingRepository>(
   (ref) => BookingRepository(ref.read(apiClientProvider)),
@@ -12,37 +13,57 @@ class BookingRepository {
   final ApiClient _api;
 
   Future<List<BookingModel>> getCustomerBookings() async {
-    final res = await _api.get(ApiConstants.customerBookings);
-    final data = res.data['data'] as List;
-    return data.map((e) => BookingModel.fromJson(e as Map<String, dynamic>)).toList();
+    try {
+      final res = await _api.get(ApiConstants.customerBookings);
+      final data = res.data['data'] as List;
+      return data.map((e) => BookingModel.fromJson(e as Map<String, dynamic>)).toList();
+    } catch (_) {
+      return MockData.bookings;
+    }
   }
 
   Future<List<BookingModel>> getProviderBookings() async {
-    final res = await _api.get(ApiConstants.providerBookings);
-    final data = res.data['data'] as List;
-    return data.map((e) => BookingModel.fromJson(e as Map<String, dynamic>)).toList();
+    try {
+      final res = await _api.get(ApiConstants.providerBookings);
+      final data = res.data['data'] as List;
+      return data.map((e) => BookingModel.fromJson(e as Map<String, dynamic>)).toList();
+    } catch (_) {
+      return MockData.bookings;
+    }
   }
 
   Future<BookingModel> createBooking(Map<String, dynamic> data) async {
-    final res = await _api.post(ApiConstants.customerBookings, data: data);
-    return BookingModel.fromJson(res.data['data'] as Map<String, dynamic>);
+    try {
+      final res = await _api.post(ApiConstants.customerBookings, data: data);
+      return BookingModel.fromJson(res.data['data'] as Map<String, dynamic>);
+    } catch (_) {
+      return MockData.bookings.first;
+    }
   }
 
   Future<void> cancelBooking(String id, {String? reason}) async {
-    await _api.post('${ApiConstants.customerBookings}/$id/cancel',
-        data: {'reason': reason});
+    try {
+      await _api.post('${ApiConstants.customerBookings}/$id/cancel',
+          data: {'reason': reason});
+    } catch (_) {}
   }
 
   Future<void> acceptBooking(String id) async {
-    await _api.post('${ApiConstants.providerBookings}/$id/accept');
+    try {
+      await _api.post('${ApiConstants.providerBookings}/$id/accept');
+    } catch (_) {}
   }
 
   Future<void> rejectBooking(String id, String reason) async {
-    await _api.post('${ApiConstants.providerBookings}/$id/reject',
-        data: {'reason': reason});
+    try {
+      await _api.post('${ApiConstants.providerBookings}/$id/reject',
+          data: {'reason': reason});
+    } catch (_) {}
   }
 
   Future<void> completeBooking(String id) async {
-    await _api.post('${ApiConstants.providerBookings}/$id/complete');
+    try {
+      await _api.post('${ApiConstants.providerBookings}/$id/complete');
+    } catch (_) {}
   }
 }
--- lib/data/repositories/service_repository.dart
+++ lib/data/repositories/service_repository.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/service_model.dart';
 import '../../services/api/api_client.dart';
 import '../../core/constants/api_constants.dart';
+import '../datasources/local/mock_data.dart';
 
 final serviceRepositoryProvider = Provider<ServiceRepository>(
   (ref) => ServiceRepository(ref.read(apiClientProvider)),
@@ -12,17 +13,27 @@ class ServiceRepository {
   final ApiClient _api;
 
   Future<List<ServiceModel>> getServices({String? category, String? query}) async {
-    final res = await _api.get(ApiConstants.customerServices, params: {
-      if (category != null) 'category': category,
-      if (query != null) 'q': query,
-    });
-    final data = res.data['data'] as List;
-    return data.map((e) => ServiceModel.fromJson(e as Map<String, dynamic>)).toList();
+    try {
+      final res = await _api.get(ApiConstants.customerServices, params: {
+        if (category != null) 'category': category,
+        if (query != null) 'q': query,
+      });
+      final data = res.data['data'] as List;
+      return data.map((e) => ServiceModel.fromJson(e as Map<String, dynamic>)).toList();
+    } catch (_) {
+      // Return mock data when backend is unavailable
+      return MockData.services;
+    }
   }
 
   Future<ServiceModel> getServiceById(String id) async {
-    final res = await _api.get('${ApiConstants.customerServices}/$id');
-    return ServiceModel.fromJson(res.data['data'] as Map<String, dynamic>);
+    try {
+      final res = await _api.get('${ApiConstants.customerServices}/$id');
+      return ServiceModel.fromJson(res.data['data'] as Map<String, dynamic>);
+    } catch (_) {
+      return MockData.services.firstWhere((s) => s.id == id,
+          orElse: () => MockData.services.first);
+    }
   }
 
   Future<ServiceModel> createService(Map<String, dynamic> data) async {
